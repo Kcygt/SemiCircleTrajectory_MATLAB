18b4935aaaecf93443dbb584fd91d9441c24e24c
KorayCAnYIGIT <KorayCAnYIGIT@hotmail.com>
2023-11-28 13:49:51 +0000

    add newversion

--- newversion/IterativeLearningControl/main.m
+++ newversion/IterativeLearningControl/main.m
@@ -1,12 +1,12 @@
+%%%% FEEDBACK SYSTEM
 %Input Generation
 Ts = 0.001;
 t = 0:Ts:0.6;
-u = zeros(size(t));
-u(t < 0.3) = 0;
-u((t >= 0.3) & (t < 0.4)) = linspace(0, 0.001, sum((t >= 0.3) & (t < 0.4)));
-u(t >= 0.4) = 0.001;
+Rj = zeros(size(t));
+Rj(t < 0.3) = 0;
+Rj((t >= 0.3) & (t < 0.4)) = linspace(0, 1, sum((t >= 0.3) & (t < 0.4)));
+Rj(t >= 0.4) = 1;
 
-% Desing Controller
 
 % Define the numerator and denominator of the systems
 plant_num = [8.331499999999999e-04 -0.002411635990000 0.001646764298800 0.001386774757085 -0.002226794738657 7.735179924151595e-04 ];
@@ -20,64 +20,42 @@ filter_den = [1 -1.25 0.5335];
 
 % Calculate the transfer functions
 plant_tf = tf(plant_num,plant_den,Ts);
-feedback_tf = tf(feedback_num,feedback_den,Ts);
+fback_tf = tf(feedback_num,feedback_den,Ts);
 filter_tf = tf(filter_num,filter_den,Ts);
-feedforward_tf = filter_tf / plant_tf;
+fforward_tf = filter_tf / plant_tf;
 
 
-system_tf = feedback(plant_tf * feedback_tf,1);
-lsim(system_tf,u,t)
+fback_tf.InputName = "e";
+fback_tf.OutputName = "u";
 
-% %%
-% sysd = plant_tf;
-% % sysd = c2d(sysc,Ts,'ZOH');
-%
-% % Get state space values for ILC
-% [Ad, Bd, Cd, Dd] = ssdata(sysd);
-%
-% % Initial condition x0, time range t - assume 1 second, pure time delay n0, relative
-% % degree r, and matrix size N
-% x0 = 0;
-% t = 0:Ts:1;
-% n0 = 0;
-% r = 1;
-% N = length(t);
-%
-% % Define input vector U and reference J - Refernce = input for this example
-% Rj = [2*ones(1,200) 1*ones(1,200) 5*ones(1,200) 3*ones(1,200) 0*ones(1,201)]';
-% U = Rj;
-%
-% % G0 not formulated as initial condition is 0
-%
-% % Formulate G
-% Gvec = zeros(N,1);
-% rVec = ((r-1):(N-n0-1))';
-%
-% for ii = 1:length(rVec)
-%     ApowVec = Ad^rVec(ii);
-%     Gvec(ii) = Cd*ApowVec*Bd;
-% end
-%
-% G = tril(toeplitz(Gvec));
-%
-% % Set up ILC
-% jmax = 10;
-%
-% l0 = 0.95; L = l0 * eye(N,N);
-% q0 = 1.00; Q = q0 * eye(N,N);
-%
-% Uj = zeros(N,1); Ujold = Uj;
-% Ej = zeros(N,1); Ejold = Ej;
-%
-% % Run ILC and plot the response for each iteration
-% for ii = 1:jmax
-%     Uj = Q*Ujold + L*Ejold;
-%     Yj = G*Uj;
-%
-%     Ej = Rj - Yj; Ej(1) = 0;
-%
-%     Ejold = Ej;
-%     Ujold = Uj;
-%     disp(Ej)
-%     plotter(ii,t,Ej,Yj,Uj,Rj,U)
-% end
+plant_tf.InputName = "u";
+plant_tf.OutputName = "y";
+
+S1 = sumblk("e = r - y");
+
+system_feedback = connect(plant_tf,fback_tf,S1,"r","y");
+
+
+
+%%% FEEDFORWARD SYSTEM
+
+fback_tf.InputName = "e";
+fback_tf.OutputName = "uc";
+
+fforward_tf.InputName = "r";
+fforward_tf.OutputName = "uf";
+
+plant_tf.InputName = "u";
+plant_tf.OutputName = "y";
+
+S1 = sumblk("e = r - y");
+S2 = sumblk("u = uf + uc");
+
+system_feedforward = connect(plant_tf,fback_tf,fforward_tf, S1,S2,"r","y");
+
+hold on;
+lsim(system_feedback,Rj,t)
+lsim(system_feedforward,Rj,t)
+legend("Feedback","Feedback + Feedforward")
+
+
